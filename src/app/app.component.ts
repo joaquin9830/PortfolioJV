0d24f7bf1eb6569774a2a05b7d6657e295ccf6ff
joaquin9830 <joaquin9811@gmail.com>
2024-01-29 13:23:55 -0300

    Ínstalación AOS | animación de scroll en componentes 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ArgPro';
+
+  ngOnInit() {
+    AOS.init()
+    window.addEventListener('load', AOS.refresh)
+  }
 }
