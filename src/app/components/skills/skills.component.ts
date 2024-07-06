4672d9ea14de453c8c23f8fac45187983db6c2bf
joaquin9830 <joaquin9811@gmail.com>
2024-07-06 20:27:21 -0300

    Sección skills con nuevos iconos | estilos | animaciones

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent {
-
+  skillsSections = [
+    {
+      name: 'Frontend',
+      skills: [
+        { name: 'Angular', image: 'assets/icons/skills/angular.png' },
+        { name: 'React', image: 'assets/icons/skills/react.png' },
+        { name: 'HTML', image: 'assets/icons/skills/html.png' },
+        { name: 'CSS', image: 'assets/icons/skills/css.png' },
+        { name: 'JavaScript', image: 'assets/icons/skills/javascript.png' },
+        { name: 'TypeScript', image: 'assets/icons/skills/typescript.png' },
+        
+      ]
+    },
+    {
+      name: 'Backend',
+      skills: [
+        { name: 'Java', image: 'assets/icons/skills/java.png' },
+        { name: 'Spring Boot', image: 'assets/icons/skills/spring.png' },
+        { name: 'Python', image: 'assets/icons/skills/python.png' },
+        { name: 'Node.js', image: 'assets/icons/skills/node-js.png' },
+       
+       
+      ]
+    },
+    {
+      name: 'DB',
+      skills: [
+        { name: 'MySQL', image: 'assets/icons/skills/mysql.png' },       
+        { name: 'PostgreSQL', image: 'assets/icons/skills/postgres.png' },
+        
+      ]
+    },
+    {
+      name: 'Tools',
+      skills: [
+        { name: 'Git', image: 'assets/icons/skills/git.png' },
+        { name: 'Github', image: 'assets/icons/skills/github.png' },
+        { name: 'Postman', image: 'assets/icons/skills/postman.png' },
+        { name: 'Figma', image: 'assets/icons/skills/figma.png' },
+        
+      ]
+    }
+  ];
 }
