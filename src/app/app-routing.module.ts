1c1a164b33749873a9db85f4350947e47ad9a11b
joaquin9830 <joaquin9811@gmail.com>
2024-07-06 23:27:55 -0300

    Nuevos iconos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EducationCrimiComponent } from './pages/education-crimi/education-crimi.component';
 import { EducationDetailComponent } from './pages/education-detail/education-detail.component';
+import { EducationAluraComponent } from './pages/education-alura/education-alura.component';
 import { HomeComponent } from './pages/home/home.component';
 
 
@@ -9,7 +10,8 @@ const routes: Routes = [
   {path:'', component:HomeComponent}, 
   {path:'home', component:HomeComponent},
   {path:'education-crimi', component:EducationCrimiComponent},
-  {path:'education-detail',component:EducationDetailComponent}
+  {path:'education-detail',component:EducationDetailComponent},
+  {path:'education-alura',component:EducationAluraComponent}
 ];
 
 @NgModule({
