077e21268d5b0635a6ba0efd85dc0be6e7d114bf
joaquin9830 <joaquin9811@gmail.com>
2024-07-29 20:40:50 -0300

    Sección servicios con iconos

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -10,17 +10,17 @@ export class ServicesComponent {
     {
       title: 'Desarrollo Frontend',
       description: 'Creamos interfaces de usuario atractivas y funcionales utilizando las últimas tecnologías.',
-      icon: 'assets/frontend-icon.png'
+      icon: 'assets/icons/services/frontend.png'
     },
     {
       title: 'Desarrollo Backend',
       description: 'Desarrollamos sistemas robustos y escalables para soportar tus aplicaciones.',
-      icon: 'assets/backend-icon.png' 
+      icon: 'assets/icons/services/backend.png' 
     },
     {
       title: 'Administración de Base de Datos',
       description: 'Gestionamos bases de datos para asegurar la integridad y disponibilidad de tus datos.',
-      icon: 'assets/database-icon.png' 
+      icon: 'assets/icons/services/database.png' 
     }
   ];
 }
