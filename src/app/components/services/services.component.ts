31de28ba64c4c787ecd8e66a5f4a8254c8af1424
joaquin9830 <joaquin9811@gmail.com>
2024-07-29 21:30:18 -0300

    Estilos responsive de servicios

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -9,17 +9,17 @@ export class ServicesComponent {
   services = [
     {
       title: 'Desarrollo Frontend',
-      description: 'Creamos interfaces de usuario atractivas y funcionales utilizando las últimas tecnologías.',
+      description: 'Creación de interfaces de usuario atractivas y funcionales.',
       icon: 'assets/icons/services/frontend.png'
     },
     {
       title: 'Desarrollo Backend',
-      description: 'Desarrollamos sistemas robustos y escalables para soportar tus aplicaciones.',
+      description: 'Desarrollo de sistemas robustos y escalables para soportar tus aplicaciones.',
       icon: 'assets/icons/services/backend.png' 
     },
     {
       title: 'Administración de Base de Datos',
-      description: 'Gestionamos bases de datos para asegurar la integridad y disponibilidad de tus datos.',
+      description: 'Gestión de bases de datos para asegurar la integridad y disponibilidad de tus datos.',
       icon: 'assets/icons/services/database.png' 
     }
   ];
